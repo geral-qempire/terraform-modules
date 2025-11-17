859e638073be063c854d422eaa7f0ac057fae725
qempire <geral.qempire@gmail.com>
2025-11-17 16:38:02 +0000

    Delete modulues

--- modules/az_action_group_map/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "action_group_id" {
-  description = "Resource ID of the action group."
-  value       = azurerm_monitor_action_group.this.id
-}
-
-output "action_group_name" {
-  description = "Name of the action group."
-  value       = azurerm_monitor_action_group.this.name
-}

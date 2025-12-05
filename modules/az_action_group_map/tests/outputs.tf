85d1f57ff6b002dc9d64c10ea9a4eeac47df7c36
geral-qempire <geral.qempire@gmail.com>
2025-12-05 16:32:00 +0000

    Dev (#16)
    * Add tests
    
    * Update
    
    * Update versions

---
+++ modules/az_action_group_map/tests/outputs.tf
@@ -0,0 +1,9 @@
+output "action_group_id" {
+  description = "Resource ID of the action group."
+  value       = module.action_group.action_group_id
+}
+
+output "action_group_name" {
+  description = "Name of the action group."
+  value       = module.action_group.action_group_name
+}
